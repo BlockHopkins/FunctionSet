c32e5e9154b05b6a23f94e767bfafe86196a30b7
BlockHopkins <1075712957@qq.com>
2019-08-24 17:08:20 +0800

    ignore

--- FunctionSet/src/com/qc/function/NaturalMonth/TestMain.java
+++ FunctionSet/src/com/qc/function/NaturalMonth/TestMain.java
@@ -1,5 +1,9 @@
 package com.qc.function.NaturalMonth;
 
+/**
+ * 日期测试 
+ *
+ */
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println(DateTimeUtils.getDayName(2));
